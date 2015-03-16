04d1b88f1e0a650ef5aabec41d565d9ad7a535cc
Lysxia <li-yao.xia@ens.fr>
2015-03-17 00:40:24 +0100

    Improve resilience to trailing punctuation

--- hurl.hs
+++ hurl.hs
@@ -24,12 +24,11 @@ import System.Exit (ExitCode(..))
 
 import Control.Lens
 -- import qualified Data.Text.ICU as ICU
-import Network.HTTP.Client (HttpException)
+import Network.HTTP.Client (HttpException(StatusCodeException))
+import Network.HTTP.Types.Status
 import Network.Wreq
 import Text.HTML.TagSoup
 import Text.PDF.Info
-
-import Debug.Trace
 
 -- Error logging to stderr
 logError :: String -> IO ()
@@ -40,19 +39,22 @@ httpOptions :: Options
 httpOptions = defaults & redirects .~ 10
 
 -- Act depending on MIME type, doing nothing if we don't know how to handle it
+--
+-- May raise HttpException.
 dispatchByHeader :: String -> Response () -> IO (Maybe T.Text)
 dispatchByHeader url response
   -- Just try UTF-8 then Latin-1. This is... not efficient.
   | contentTypeIs "text/html" = do
       html <- body
-      first $ forceTitle <$> (getHTMLTitle =<<)
-        <$> ([(decodeUtf8 <$>), (decodeLatin1 <$>)] <*> pure html)
+      first $ forceTitle <$> getHTMLTitle
+        <$> ([decodeUtf8, decodeLatin1] <*> pure html)
   | contentTypeIs "application/pdf" || contentTypeIs "application/x-pdf" =
-    (maybe (return Nothing) getPDFTitle) =<< body
+      getPDFTitle =<< body
   | otherwise = return Nothing
   where contentTypeIs = flip BS.isPrefixOf . normalize $ response ^. responseHeader "Content-Type"
-        body = ((^. responseBody) <$>) <$> requestMaybe (getWith httpOptions url)
+        body = (^. responseBody) <$> httpGet url
         normalize = BS.map W8.toLower . BS.filter (not . W8.isSpace)
+        httpGet = getWith httpOptions
 
 -- Evaluate the title while catching decoding exceptions
 forceTitle :: Maybe T.Text -> IO (Maybe T.Text)
@@ -109,7 +111,7 @@ getPDFTitle body = do
   eitherInfo <- pdfInfo tmpPDF
   case eitherInfo of
     Right info -> case pdfInfoTitle info of
-      Just txt | not (T.null txt) -> traceShow txt $ return . Just $ txt
+      Just txt | not (T.null txt) -> return . Just $ txt
       _ -> do
         -- TODO: handle exceptions
         exitCode <- system $ "pdftotext -layout -f 1 -l 1 " ++ tmpPDF
@@ -122,25 +124,46 @@ getPDFTitle body = do
     tmpPDF = tmp ++ ".pdf"
     tmpTXT = tmp ++ ".txt"
 
-requestMaybe :: IO a -> IO (Maybe a)
-requestMaybe req = handle handleException $ Just <$> req
-  where handleException :: HttpException -> IO (Maybe a)
+type HttpResult = Either HttpException
+
+-- Catch HTTP errors
+handleHttp :: IO a -> IO (HttpResult a)
+handleHttp req = handle handleException $ Right <$> req
+  where handleException :: HttpException -> IO (HttpResult a)
         handleException x = do
           logError (show x)
-          return Nothing
+          return (Left x)
+
+-- Execute the IO action if the error was 404 (Not Found), just fail otherwise.
+tryOn404 :: HttpResult (Maybe a) -> IO (Maybe a) -> IO (Maybe a)
+tryOn404 (Right x) _ = return x
+tryOn404 (Left (StatusCodeException (Status 404 _) _ _)) retry = retry
+tryOn404 (Left _) _ = return Nothing
 
 -- Fetch the title of a given URL (if any…)
+--
+-- May raise HttpException
 fetchTitle :: String -> IO (Maybe T.Text)
 fetchTitle url = do
   -- Initially, only do HEAD, defer GET to when we know we want the document
-  response <- requestMaybe $ headWith httpOptions url
-  join <$> forM response (dispatchByHeader url)
+  dispatchByHeader url =<< httpHead url
+  where httpHead = headWith httpOptions
+
+-- Wrapper that catches HttpExceptions while trying fetchTitle
+-- with at most two different URL candidates.
+fetchAndRetry :: String -> IO (Maybe T.Text)
+fetchAndRetry url = do
+  t <- fetch url
+  tryOn404 t $ retry
+  where fetch = handleHttp . fetchTitle
+        hush' = join . hush
+        retry = logError "Retrying..." >> hush' <$> fetch (init url)
 
 main :: IO ()
 main = forever $ do
   messageURL <- getURL <$> getLine
   forM_ messageURL $ \url -> do
-    title <- fetchTitle url
+    title <- fetchAndRetry url
     forM_ title $ \t -> do
       forM_ (
         take 3 .
